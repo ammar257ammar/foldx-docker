2aa6eb1191e6afafcce31bffb6fcbc6d0f4f7503
ammar257ammar <43293485+ammar257ammar@users.noreply.github.com>
2019-12-05 17:24:47 +0100

    fix typo

--- foldx.sh
+++ foldx.sh
@@ -22,10 +22,8 @@ for f in *; do
 	elif [ "$COMMAND" == "BuildModel" ]; then
 
 		cd $f/input
-
-			cd $f/input
 	
-			/foldx/foldx --command=BuildModel --pdb=${f}_protein_Repair.pdb --mutant-file=individual_list.txt --ionStrength=0.05 --pH=7 --water=CRYSTAL --vdwDesign=2 --output-dir=../output --out-pdb=true --pdbHydrogens=false --numberOfRuns=5 > ../output/log.txt &
+		/foldx/foldx --command=BuildModel --pdb=${f}_protein_Repair.pdb --mutant-file=individual_list.txt --ionStrength=0.05 --pH=7 --water=CRYSTAL --vdwDesign=2 --output-dir=../output --out-pdb=true --pdbHydrogens=false --numberOfRuns=5 > ../output/log.txt &
 
 		cd ../..
 	fi
